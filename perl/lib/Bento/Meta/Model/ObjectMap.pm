6570c0387c2aa2649fe57d6a0ac1424219ba1d03
Mark A. Jensen <mark.jensen@nih.gov>
2020-04-12 08:37:57 -0400

    feat(put_attr) - WIP

--- perl/lib/Bento/Meta/Model/ObjectMap.pm
+++ perl/lib/Bento/Meta/Model/ObjectMap.pm
@@ -68,7 +68,7 @@ sub get_q {
   
   if ($obj->neoid) {
     return cypher->match('n:'.$self->label)
-      ->where({'id(n)' => $obj->neoid })
+      ->where( { 'id(n)' => $obj->neoid })
       ->return('n');
   }
   # else, find equivalent node
@@ -112,5 +112,130 @@ sub get_attr_q {
   }
 }
 
+# put_q
+# if obj has a neoid (is 'mapped'), then overwrite the props on that node
+# in the DB with the props in the obj
+# if obj does not have a neoid, create a new node with the props on the object
+# both stmts return the node id
+sub put_q {
+  my $self = shift;
+  my ($obj) = @_;
+  unless ( blessed $obj && $obj->isa($self->class) ) {
+    LOGDIE ref($self)."::put_q : arg1 must be an object of class ".$self->class;
+  }
+  my $props = {};
+  my @null_props;
+  for ($self->property_attrs) {
+    if (defined $obj->$_) {
+      $props->{$self->pmap->{$_}} = $obj->$_;
+    }
+    else {
+      push @null_props, $self->pmap->{$_};
+    }
+  }
   
+  if ($obj->neoid) {
+  # rewrite props on existing node
+  # need to set props that have defined values,
+  # and remove props that undefined values -
+  # so 2 statements are returned
+    my @stmts;
+    push @stmts, cypher->match('n:'.$self->label)
+      ->where({ 'id(n)' => $obj->neoid })
+      ->set($props)
+      ->return('id(n)');
+    for (@null_props) {
+      push @stmts, cypher->match('n:'.$self->label)
+        ->where({ 'id(n)' => $obj->neoid })
+        ->remove('n.'.$_)
+        ->return('id(n)');
+    }
+    return @stmts;
+  }
+  # else, create new node with props that have defined values
+  return cypher->create(ptn->N('n:'.$self->label => $props))
+    ->return('id(n)');
+}
+
+# put_attr_q($obj, $attr => @values)
+# - query to create relationships and end nodes corresponding
+# to object- or collection-valued attributes
+# can only do this on node that already is mapped in the db (must have non-empty
+# neoid() attr)
+# for each obj in @values - require these all be present in db as well? Or create de novo?
+# - require that they be mapped: do put_q($_) for (@values), then put_attr_q,
+# if necessary
+sub put_attr_q {
+  my $self = shift;
+  my ($obj,$att, @values) = @_;
+  unless ( blessed $obj && $obj->isa($self->class) ) {
+    LOGDIE ref($self)."::put_attr_q : arg1 must be an object of class ".$self->class;
+  }
+  unless (defined $obj->neoid) {
+    LOGDIE ref($self)."::put_attr_q : arg1 must be a mapped object (attr 'neoid' must be set)";  }
+  unless (@values) {
+    LOGDIE ref($self)."::put_attr_q : arg2,... must be a list of endpoint objects";
+  }
+  if ( grep /^$att$/, $self->property_attrs ) {
+    return cypher->match('n:'.$self->label)
+      ->where({'id(n)' => $obj->neoid })
+      ->set( {'n.'.$self->pmap->{$att} => $values[0]} )
+      ->return('id(n)');
+  }
+  elsif (grep /^$att$/, $self->relationship_attrs ) {
+    my ($reln, $end_label, $many) = @{$self->rmap->{$att}};
+    my @stmts;
+    for my $val (@values) {
+      unless (blessed $val && $val->isa('Bento::Meta::Model::Entity')) {
+        LOGDIE ref($self)."::put_attr_q : arg 3,... must all be Entity objects";
+      }
+      unless ($val->neoid) {
+        LOGDIE ref($self)."::put_attr_q : arg 3,... must all be mapped objects (all must have 'neoid' set)";
+      }
+      my $q = cypher->match('n:'.$self->label)
+        ->where({'id(n)' => $obj->neoid })
+        ->with('n')
+        ->merge(ptn->N('n')->R(":$reln")->N('a:'.$end_label))
+        ->where({'id(a)' => $val->neoid})
+        ->return('id(a)');
+      push @stmts, $q;
+      last unless $many;
+    }
+    return @stmts;
+  }
+  else {
+    LOGDIE ref($self)."::put_attr_q : '$att' is not a registered attribute for class ".$self->class;
+  }
+}
+
+=head1 NAME
+
+Bento::Meta::Model::ObjectMap - interface Perl objects with Neo4j database
+
+=head1 SYNOPSIS
+
+  # create object map for class
+  $map = Bento::Meta::Model::ObjectMap->new('Bento::Meta::Model::Node')
+  # map object attributes to Neo4j model 
+  #  simple attrs = properties
+  for my $p (qw/handle model category/) {
+    $map->map_simple_attr($p => $p);
+  }
+  #  object- or collection-valued attrs = relationships to other nodes
+  $map->map_object_attr('concept' => 'has_concept', 'concept');
+  $map->map_collection_attr('props' => 'has_property', 'property');
+
+  # use the map to generate canned cypher queries
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+ Mark A. Jensen < mark -dot- jensen -at- nih -dot- gov >
+ FNL
+
+=cut
+
 1;
--- perl/t/006_map.t
+++ perl/t/006_map.t
@@ -13,6 +13,10 @@ throws_ok { Bento::Meta::Model::ObjectMap->new() } qr/object class/, 'check new(
 ok my $map = Bento::Meta::Model::ObjectMap->new('Bento::Meta::Model::Node'), 'create object map';
 isa_ok($map, 'Bento::Meta::Model::ObjectMap');
 
+my $concept = Bento::Meta::Model::Concept->new();
+$concept->set_id($concept->make_uuid);
+throws_ok { $map->get_q($concept) } qr/arg1 must be an object of class/, 'get_q throws when arg has wrong class';
+
 for my $p (qw/handle model category/) {
   ok $map->map_simple_attr($p => $p), 'map simple attr';
 }
@@ -42,6 +46,22 @@ throws_ok { $map->get_attr_q($node, 'frelb') } qr/not a registered attr/, "throw
 ok my $simple_q = $map->get_attr_q($node, 'handle'), "simple attr";
 ok my $object_q = $map->get_attr_q($node, 'concept'), "object attr";
 ok my $collect_q = $map->get_attr_q($node, 'props') , "collection attr";
+
+$node->set_neoid(undef);
+my $put_q = $map->put_q($node);
+ok( ($put_q->as_string eq "CREATE (n:node {handle:'test',model:'test_model'}) RETURN id(n)") or ($put_q->as_string eq  "CREATE (n:node {model:'test_model',handle:'test'}) RETURN id(n)"), "put (new node) correct");
+$node->set_neoid(1);
+my @upd_q = $map->put_q($node);
+is scalar @upd_q, 2, "two put (existing node) queries";
+ok( ($upd_q[0]->as_string eq "MATCH (n:node) WHERE (id(n) = 1) SET handle = 'test',model = 'test_model' RETURN id(n)") or ($upd_q[0]->as_string eq "MATCH (n:node) WHERE (id(n) = 1) SET model = 'test_model',handle = 'test' RETURN id(n)"), "set props query correct");
+is $upd_q[1]->as_string, "MATCH (n:node) WHERE (id(n) = 1) REMOVE n.category RETURN id(n)", "rm props query correct";
+
+$node->set_neoid(undef);
+throws_ok { $map->put_attr_q($node) } qr/must be a mapped object/, "throw if obj not mapped";
+$node->set_neoid(1);
+throws_ok { $map->put_attr_q($node,'concept' => $concept) } qr/must all be mapped objects/, "throw if value not mapped";
+$concept->set_neoid(2);
+throws_ok {  $map->put_attr_q($node,'concept' => 'boog') } qr/must all be Entity objects/, "throw if not all values are objects";
 
 $DB::single=1;
 done_testing;
