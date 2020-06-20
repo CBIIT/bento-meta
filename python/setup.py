015f39f2470b0919e5471ae03810f846affc5b71
Mark A. Jensen <maj-git@fortinbras.us>
2020-06-20 18:45:28 -0400

    feat(py): build tools
    setup.py and tox.ini

---
+++ python/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+  name="bento-meta",
+  version="0.0.1",
+  packages=find_packages(),
+  install_requires=[
+    'PyYAML>=5.1.1',
+    'option-merge>=1.6',
+    'neo4j>=4.0'
+    ],
+  tests_require=[
+    'pytest'
+    ]
+  )
