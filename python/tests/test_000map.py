e174fe6abcd6b7680e90b62c33f3c1af0ea4e33e
bensonml <bensonml@nih.gov>
2020-07-28 16:58:59 -0400

    tests for adding then removing attr

--- python/tests/test_000map.py
+++ python/tests/test_000map.py
@@ -130,5 +130,68 @@ def test_rm_queries():
   assert len(stmts) == 3
 
 
-  
-    
+def test_put_then_rm_queries():
+  """test adding then removing attr"""
+  m = ObjectMap(cls=Node)
+  n = Node({"handle":"test_","model":"test_model_","category":1})
+  qry = m.put_q(n)
+  assert qry==['CREATE (n:node {handle:"test_",model:"test_model_",category:1}) RETURN n,id(n)']
+
+  # manually set neoid
+  n.neoid=2
+  stmts = m.put_q(n)
+  assert stmts[0]=='MATCH (n:node) WHERE id(n)=2 SET n.handle="test_",n.model="test_model_",n.category=1 RETURN n,id(n)'
+  assert len(stmts[1:]) == len([x for x in Node.attspec if Node.attspec[x] == 'simple'])-3
+  for s in stmts[1:]:
+    assert re.match('^MATCH \\(n:node\\) WHERE id\\(n\\)=2 REMOVE n.[a-z_]+ RETURN n,id\\(n\\)$',s)
+
+  n.neoid=None
+  with pytest.raises(ArgError, match='object must be mapped'):
+    m.put_attr_q(n,'category',2)
+
+  n.neoid=1
+  c = Concept({"_id":"blarfblark"})
+  with pytest.raises(ArgError, match="'values' must be a list of mapped Entity objects"):
+    m.put_attr_q(n,'concept',c)
+  with pytest.raises(ArgError, match="'values' must be a list of mapped Entity objects"):  
+    m.put_attr_q(n,'concept',[c])
+
+  qry = m.put_attr_q(n,'category',[3])
+  assert qry=="MATCH (n:node) WHERE id(n)=1 SET category=3 RETURN id(n)"
+
+  c.neoid=2
+  stmts = m.put_attr_q(n,'concept',[c])
+  #assert stmts[0] == "MATCH (n:node),(a:concept) WHERE id(n)=1 AND id(a)=2 MERGE (n)-[:has_concept]->(a) RETURN id(a)"
+  assert len(stmts) == 1
+
+  prps = [Property(x) for x in ( {"model":"test_","handle":"prop1"},
+                                 {"model":"test_","handle":"prop2"},
+                                 {"model":"test_","handle":"prop3"} )]
+  i=5
+  for p in prps:
+    p.neoid=i
+    i+=1
+  stmts = m.put_attr_q(n,'props',prps)
+  assert stmts[0] == "MATCH (n:node),(a:property) WHERE id(n)=1 AND id(a)=5 MERGE (n)-[:has_property]->(a) RETURN id(a)"
+  assert len(stmts) == 3
+  Node.mapspec_={"label":"node",
+             "property": {"handle":"handle","model":"model","category":"category"},
+             "relationship": {
+               "concept": { "rel" : ":has_concept>",
+                            "end_cls" : {"Concept","Term"} },
+               "props": { "rel" : ":has_property>",
+                          "end_cls" : "Property" }
+               }}
+  (Node.attspec, Node._mapspec) = mergespec('Node',Node.attspec,Node.mapspec_)
+  assert Node.mapspec()["relationship"]["concept"]["end_cls"] == {"Concept","Term"}
+  t = Term({"value":"boogblark"})
+  t.neoid=6
+  stmts = m.put_attr_q(n,"concept",[t])
+  assert re.match("MATCH \\(n:node\\),\\(a\\) WHERE id\\(n\\)=1 AND id\\(a\\)=6 AND \\('[a-z]+' IN labels\\(a\\) OR '[a-z]+' IN labels\\(a\\)\\) MERGE \\(n\\)-\\[:has_concept\\]->\\(a\\) RETURN id\\(a\\)",stmts[0])
+  qry = m.get_attr_q(n,"concept")
+  assert re.match("MATCH \\(n:node\\)-\\[:has_concept\\]->\\(a\\) WHERE id\\(n\\)=1 AND \\('[a-z]+' IN labels\\(a\\) OR '[a-z]+' IN labels\\(a\\)\\) RETURN a",qry)
+
+  # now delete the attr I just added....
+  qry2 = m.rm_attr_q(n, "concept", [t])
+  assert re.match("MATCH \\(n:node\\)-\\[r:has_concept\\]->\\(a\\) WHERE id\\(n\\)=1 AND id\\(a\\)=6 AND \\('[a-z]+' IN labels\\(a\\) OR '[a-z]+' IN labels\\(a\\)\\) DELETE r RETURN id\\(n\\),id\\(a\\)",qry2[0])
+
